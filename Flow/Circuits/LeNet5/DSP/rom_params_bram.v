ad6435d3466f293fc14d6853a0ada57125c1c89a
haozhang <haozhang@mail.sdu.edu.cn>
2022-12-22 04:13:26 +0000

    update

--- Flow/Circuits/LeNet5/DSP/rom_params_bram.v
+++ Flow/Circuits/LeNet5/DSP/rom_params_bram.v
@@ -11,8 +11,7 @@ module rom_params_bram #(parameter BIT_WIDTH = 8, SIZE = 26) (
     // input [clogb2(SIZE)-1:0] addr,
 	output [BIT_WIDTH*SIZE-1:0] out
 );
-    defparam weight_single_port_ram.ADDR_WIDTH = clogb2(SIZE);
-    defparam weight_single_port_ram.DATA_WIDTH = BIT_WIDTH*SIZE;
+
 
 	reg done_flag;
 	reg [clogb2(SIZE)-1:0] add_counter;
@@ -28,10 +27,13 @@ module rom_params_bram #(parameter BIT_WIDTH = 8, SIZE = 26) (
 		end
 	end
 
+
+    defparam weight_single_port_ram.ADDR_WIDTH = clogb2(SIZE);
+    defparam weight_single_port_ram.DATA_WIDTH = BIT_WIDTH*SIZE;
     single_port_ram weight_single_port_ram(
     .addr(add_counter),
     .we(!read),
-    .data(BIT_WIDTH*SIZE'b0),
+    .data(BIT_WIDTH*SIZE),
     .out(out),
     .clk(clk)
     );
