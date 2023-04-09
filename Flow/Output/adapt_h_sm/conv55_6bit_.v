d32129c265a13ba7b77c9db79a73dbb145f3661b
haozhang <haozhang@mail.sdu.edu.cn>
2023-04-09 12:35:34 +0000

    rewrite conv op for clb and dsp

--- Flow/Output/adapt_h_sm/conv55_6bit_.v
+++ Flow/Output/adapt_h_sm/conv55_6bit_.v
@@ -17,7 +17,7 @@ module multiplier(input [5:0] a, b, output [11:0] p);
 endmodule
 
 
-module conv55_6bit (
+module conv55_6bit_ (
   input [5:0] in_data_0, input [5:0] in_data_1, input [5:0] in_data_2, input [5:0] in_data_3, input [5:0] in_data_4, input [5:0] in_data_5, input [5:0] in_data_6, input [5:0] in_data_7, input [5:0] in_data_8, input [5:0] in_data_9, input [5:0] in_data_10, input [5:0] in_data_11, input [5:0] in_data_12, input [5:0] in_data_13, input [5:0] in_data_14, input [5:0] in_data_15, input [5:0] in_data_16, input [5:0] in_data_17, input [5:0] in_data_18, input [5:0] in_data_19, input [5:0] in_data_20, input [5:0] in_data_21, input [5:0] in_data_22, input [5:0] in_data_23, input [5:0] in_data_24, // 输入数据
   input [5:0] kernel_0, input [5:0] kernel_1, input [5:0] kernel_2, input [5:0] kernel_3, input [5:0] kernel_4, input [5:0] kernel_5, input [5:0] kernel_6, input [5:0] kernel_7, input [5:0] kernel_8, input [5:0] kernel_9, input [5:0] kernel_10, input [5:0] kernel_11, input [5:0] kernel_12, input [5:0] kernel_13, input [5:0] kernel_14, input [5:0] kernel_15, input [5:0] kernel_16, input [5:0] kernel_17, input [5:0] kernel_18, input [5:0] kernel_19, input [5:0] kernel_20, input [5:0] kernel_21, input [5:0] kernel_22, input [5:0] kernel_23, input [5:0] kernel_24,// 卷积核
   input clk,
@@ -56,12 +56,12 @@ module conv55_6bit (
 
 
 
-  parallel_adder_tree adder_tree_inst(.a(conv_sum), .clk(clk), .sum(out_data));
+  parallel_adder_tree_ adder_tree_inst(.a(conv_sum), .clk(clk), .sum(out_data));
 
 
 endmodule
 
-module parallel_adder_tree (
+module parallel_adder_tree_ (
     input [299:0] a,  // 25个16位数字输入
     input clk,
     output reg [17:0] sum // 结果
